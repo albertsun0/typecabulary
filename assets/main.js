178fea6576c476ca5a4ed85c06c6e17ec63ab625
qwertyuiop888 <albertsun0@gmail.com>
2020-10-26 14:24:44 -0500

    ui fixes

--- assets/main.js
+++ assets/main.js
@@ -44,17 +44,12 @@ function stoptimer(){
 }
 
 var currentword = "start";
-var nextword = "";
+var nextword = words[Math.floor(Math.random() * words.length)];
 
 function beginPractice() {
 	console.log("start");
 	start = true;
-	document.getElementById('backButton').style.visibility = "visible";
-	document.getElementById('startPracticeButton').style.display = "none";
-	document.getElementById('startSpeedrunButton').style.display = "none";
-	document.getElementById('inputbox').value = "";
-	starttimer();
-	document.getElementById("inputbox").focus();
+	begin();
 }
 /*window.onload=function(){
   const box = document.getElementById('inputbox');
@@ -66,13 +61,24 @@ function beginSpeedrun(){
 	console.log("Start speedrun");
 	timeElapsed =speedrunTime;
 	startSpeedrun = true;
-	document.getElementById('backButton').style.visibility = "visible";
+	begin();
+	
+	document.getElementById("time").innerHTML = Math.floor(speedrunTime/60) +':' +("0" + (speedrunTime%60)).slice(-2);
+}
+
+function begin(){
+
+	for(var i=0; i<length-2; i++){ //clears the list of completed words
+		document.getElementById("wordlog").removeChild(document.getElementById("wordlog").lastChild);
+	}
+
+	document.getElementById('backButton').style.display = "inline";
 	document.getElementById('startPracticeButton').style.display = "none";
 	document.getElementById('startSpeedrunButton').style.display = "none";
 	document.getElementById('inputbox').value = "";
 	starttimer();
 	document.getElementById("inputbox").focus();
-	document.getElementById("time").innerHTML = Math.floor(speedrunTime/60) +':' +("0" + (speedrunTime%60)).slice(-2);
+	document.getElementById("nextword").innerHTML = nextword;
 }
 
 function checktext(){
@@ -104,9 +110,11 @@ function checktext(){
 }
 
 function newword(){
-	currentword = words[Math.floor(Math.random() * words.length)];
+	currentword = nextword;
+	nextword = words[Math.floor(Math.random() * words.length)];
 	console.log(currentword);
 	document.getElementById("word").innerHTML = currentword;
+	document.getElementById("nextword").innerHTML = nextword;
 }
 
 function correctadd(){
@@ -130,13 +138,10 @@ function incorrectadd(){
 function backButton(){
 	stoptimer();
 	var length = document.getElementById("wordlog").children.length;
-	for(var i=0; i<length-2; i++){ //clears the list of completed words
-		document.getElementById("wordlog").removeChild(document.getElementById("wordlog").lastChild);
-	}
 	
 	document.getElementById('startPracticeButton').style.display = "inline";
 	document.getElementById('startSpeedrunButton').style.display = "inline";
-	document.getElementById('backButton').style.visibility = "hidden";
+	document.getElementById('backButton').style.display = "none";
 	document.getElementById('inputbox').value = "";
 	document.getElementById("time").innerHTML = '0:00';
 	document.getElementById("word").innerHTML = "start";
@@ -145,5 +150,5 @@ function backButton(){
 	document.getElementById("correct").innerHTML = '0 correct';
 	currentword = "start";
 	right = 0;
-	wrong =0;
+	wrong = 0;
 }
